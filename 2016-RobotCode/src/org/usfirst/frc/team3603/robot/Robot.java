fee3986b86a5caca890b3142fbd1f9fb0371768b
mrvanscoyoc <mr.vanscoyoc@gmail.com>
2016-02-20 08:20:13 -0500

    updated buttons and additional comments

--- 2016-RobotCode/src/org/usfirst/frc/team3603/robot/Robot.java
+++ 2016-RobotCode/src/org/usfirst/frc/team3603/robot/Robot.java
@@ -1,9 +1,10 @@
 
 /****************************************
-**	2016 FRC Season
-**	Iterative Based JAVA 
-**	CyberCoyotes
-****************************************/
+ * 
+ *	Iterative Based JAVA for 2016 FRC Season
+ *	@author CyberCoyotes
+ *
+ ****************************************/
 
 package org.usfirst.frc.team3603.robot;
 
@@ -20,6 +21,7 @@ import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
+/* Assigns speed controllers to names and PWN numbers */
 	
 	Joystick xbox = new Joystick(0);					// XBOX controller for driving - USB 0
 
@@ -28,25 +30,25 @@ public class Robot extends IterativeRobot {
 	Victor right = new Victor(1);						// PWN channel 1 for right motor drive train
 
 	RobotDrive maindrive = new RobotDrive(left, right);
-	
+
 	Joystick xbox2 = new Joystick(1);					// XBOX controller for manipulating - USB 1
-	
+
 	Talon lift = new Talon(2);							// scissor lift screw drive motor
-	
+
 	Talon shooter = new Talon(3); 						// shooter wheels
-	
+
 	Victor winch = new Victor(4); 						// CIM winch 1 motor
-	
+
 	Victor winch2 = new Victor(5); 						// CIM winch 2 motor
-	
+
 	//	Victor arm = new Victor(5);						// Arm manipulator motor - REPLACED WITH CIM 2016-02-19
-	
+
 	Compressor single = new Compressor();
-	
+
 	DoubleSolenoid doublesol2 = new DoubleSolenoid(5,6);
-	
+
 	DoubleSolenoid doublesol = new DoubleSolenoid(1, 2);
-	
+
 	Timer Timer = new Timer();
 
 	public void robotInit() {
@@ -54,18 +56,27 @@ public class Robot extends IterativeRobot {
 		Timer.start();
 	}
 
-	/* Autonomous Section of Code */
 
+	/*	AUTONONMOUS Code */
+	//	GOAL 1: Drive under low bar with a ball 
+	//	GOAL 2: Turn and target goal
+	//	GOAL 3: Shoot ball for a score
+	//
+	//	ASSUMPTIONS
+	//	Driving under the low bar
+	//	Shooting (High or Low?)
 	public void autonomousPeriodic() {
-		
-														// GOAL 1: Drive under low bar with a ball 
-														// GOAL 2: Turn and target high OR low goal
-														// GOAL 3: Shoot ball for a score
+
 	}
+
+	/****************************************
+	 *	TELEOP Code
+	 *
+	 ****************************************/
 
 	public void teleopPeriodic() {
 		while (isOperatorControl() && isEnabled()) {
-			
+
 			/* XBOX 1 DRIVER CODE */
 			SmartDashboard.putNumber("Timer value", Timer.get());
 			double xboxmagnitude1 = xbox.getRawAxis(1);
@@ -73,78 +84,78 @@ public class Robot extends IterativeRobot {
 
 			double all = xboxmagnitude1 + xboxmagnitude5;
 
-			if (all > .2 || all < -.2) { // ||
+			if (all > .2 || all < -.2) { 				// || what does this code do? 
 				maindrive.tankDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(5));
 			}
 			SmartDashboard.putNumber("Timer value", Timer.get());
 
-			
-			
+
+
 			if (xbox.getRawButton(3)) { 				// X BUTTON Drive Forward
 				maindrive.tankDrive(.5, .5);
 			}
-			
+
 			if (xbox.getRawButton(4)) { 				// Y BUTTON Drive backward
 				maindrive.tankDrive(-.5, -.5);
 			}
-			
-			if (xbox.getRawButton(1)) { 				// A BUTTON Spin slow left
+
+			if (xbox.getRawButton(1)) { 				// A BUTTON Spin slow left DO WE NEED THIS?
 				maindrive.tankDrive(-.5, .5);
 			}
-			
-			if (xbox.getRawButton(2)) { 				// B BUTTON Spin slow right
+
+			if (xbox.getRawButton(2)) { 				// B BUTTON Spin slow right DO WE NEED THIS?
 				maindrive.tankDrive(.5, -.5);
 			}
-			
-			if (xbox.getRawButton(6)) { 				// BUMPER RIGHT Drive right forward
+
+			if (xbox.getRawButton(6)) { 				// BUMPER RIGHT Drive right side forward, turn left
 				maindrive.tankDrive(1.0, -1.0);
 			}
-			
-			if (xbox.getRawButton(5)) {					// BUMPER LEFT Drive left forward
+
+			if (xbox.getRawButton(5)) {					// BUMPER LEFT Drive left side forward, turn right
 				maindrive.tankDrive(-1.0, 1.0);
 			}
-			
+
 			if (xbox.getRawAxis(3) > 0.0) {				// TRIGGER RIGHT Drive right side forward with variable speed at axis command
 				maindrive.tankDrive(0, xbox.getRawAxis(3));
 				if (xbox.getRawAxis(2) > 0.0) {
 					maindrive.tankDrive(1.0, 1.0);
 				}
 			}
-			
-			
+
+
 			if (xbox.getRawAxis(2) > 0.0) {				// TRIGGER LEFT Drive left side forward with variable speed at axis command
 				maindrive.tankDrive(xbox.getRawAxis(2), 0);
 				if (xbox.getRawAxis(3) > 0.0) {			
 					maindrive.tankDrive(1.0, 1.0);
 				}
 			}			
-			
+
 			/* XBOX 2 Manipulator Code*/
-														// X BUTTON future code for tomahawk?
-			
-														// Y BUTTON future code for tomahawk?
-			
-			
-			 if (xbox2.getRawButton(1)) {				// A BUTTON Scissor Lift Motor DOWN
-					lift.set(-1.0); 
-					SmartDashboard.putNumber("lift value", lift.get());
-			
-			 } else if (xbox2.getRawButton(2)) {		// B BUTTON Scissor Lift Motor UP
-				 lift.set(.75); 
-				 SmartDashboard.putNumber("lift value", lift.get());
-		
+			// X BUTTON future code for tomahawk piston
+
+			// Y BUTTON future code for tomahawk piston
+
+
+			if (xbox2.getRawButton(1)) {				// A BUTTON Scissor Lift Motor UP
+				lift.set(.5); 
+				SmartDashboard.putNumber("lift value", lift.get());
+
+			} else if (xbox2.getRawButton(2)) {		// B BUTTON Scissor Lift Motor DOWN
+				lift.set(-.75); 
+				SmartDashboard.putNumber("lift value", lift.get());
+
 			} else {
 				lift.stopMotor();
 				SmartDashboard.putNumber("lift value", lift.get());
 
 			}
-			
+
 			/* First winch CIM  */
-			if (xbox2.getRawButton(8)) { 				// START BUTTON
+			if (xbox2.getRawButton(8)) { 				// START BUTTON check to see! Should be Set to pull the robot up shou
 				winch.set(.5);
 				SmartDashboard.putNumber("winch value", winch.get());
-				
-			} else if (xbox2.getRawButton(7)) { 		// BACK BUTTON 
+
+			} else if (xbox2.getRawButton(7)) { 		// BACK BUTTON reverse winch motor, should be needed other than winding rope
 				winch.set(-.5);
 				SmartDashboard.putNumber("winch value", winch.get());
 
@@ -153,12 +164,12 @@ public class Robot extends IterativeRobot {
 				SmartDashboard.putNumber("winch value", winch.get());
 
 			}
-			
+
 			/* Second Winch CIM  */
 			if (xbox2.getRawButton(8)) { 				// START BUTTON
 				winch2.set(.5);
 				SmartDashboard.putNumber("winch2 value", winch.get());
-				
+
 			} else if (xbox2.getRawButton(7)) { 		// BACK BUTTON
 				winch2.set(-.5);
 				SmartDashboard.putNumber("winch2 value", winch.get());
@@ -171,22 +182,22 @@ public class Robot extends IterativeRobot {
 
 			if (xbox2.getRawButton(6)) { 				// RIGHT BUMPER double solenoid forward
 				doublesol.set(DoubleSolenoid.Value.kForward);
-				
+
 			} else if (xbox2.getRawButton(5)) { 		// LEFT BUMPER double solenoid reverse
 				doublesol.set(DoubleSolenoid.Value.kReverse);
 			}
-			
+
 			if (xbox2.getRawButton(9)) { 				// THUMBSTICK BUTTON DOWN double solenoid forward
 				doublesol2.set(DoubleSolenoid.Value.kForward);
-				
+
 			} else if (xbox2.getRawButton(10)) { 		// THUMBSTICK BUTTON DOWN double solenoid reverse
 				doublesol2.set(DoubleSolenoid.Value.kReverse);
 			}
-			
+
 			if (xbox2.getRawAxis(2) > 0.0) {			// RIGHT BUMPER
 				shooter.set(xbox2.getRawAxis(2));
 				SmartDashboard.putNumber("shooter value", shooter.get());
-				
+
 			} else if (xbox2.getRawAxis(3) > 0.0) {		// LEFT BUMPER
 				shooter.set(-xbox2.getRawAxis(3));
 				SmartDashboard.putNumber("shooter value", shooter.get());
@@ -196,19 +207,19 @@ public class Robot extends IterativeRobot {
 				SmartDashboard.putNumber("shooter value", shooter.get());
 
 			}
-			
-	// CURRENTLY ARM NO LONGER ATTACHED 2016-02-16			
-	//		if (xbox2.getRawButton(4)) { 				// Y BUTTON arm motor UP
-	//			arm.set(.5);
-	//			SmartDashboard.putNumber("arm value", arm.get());
-	//		} else if (xbox2.getRawButton(3)) { 		// X BUTTON arm motor DOWN
-	//			arm.set(-.5);
-	//			SmartDashboard.putNumber("arm value", arm.get());
-	//		} else {
-	//			arm.stopMotor();
-	//			SmartDashboard.putNumber("arm value", arm.get());
-	//		}
-	//
+
+			// CURRENTLY ARM NO LONGER ATTACHED 2016-02-16			
+			//		if (xbox2.getRawButton(4)) { 				// Y BUTTON arm motor UP
+			//			arm.set(.5);
+			//			SmartDashboard.putNumber("arm value", arm.get());
+			//		} else if (xbox2.getRawButton(3)) { 		// X BUTTON arm motor DOWN
+			//			arm.set(-.5);
+			//			SmartDashboard.putNumber("arm value", arm.get());
+			//		} else {
+			//			arm.stopMotor();
+			//			SmartDashboard.putNumber("arm value", arm.get());
+			//		}
+			//
 		}
 	}
 
